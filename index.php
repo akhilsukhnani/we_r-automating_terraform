15d41c81e42adc3f6f6ac68300451a98f5141752
akhilsukhnani <63036628+akhilsukhnani@users.noreply.github.com>
2020-06-12 23:07:03 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<body bgcolor='aqua'>
+<marquee>TERRAFORM IS A GREAT TOOL GUYS </marquee>
+<h1>WELCOME TO MY WEBPAGE</h1>
+<p>and this is the  ip address of the instance where its is deployed</p>
+<br/>
+<pre>
+
+<?php>
+print `ifconfig`
+?>
+</body>
+</pre>
