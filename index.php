f88bd715fb77e1df8412424c23c828f72db69b3e
akhilsukhnani <63036628+akhilsukhnani@users.noreply.github.com>
2020-06-13 18:34:15 +0530

    frgergrg

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body bgcolor='blue'>
 <marquee><h1>After completing the task i was like !</h1></marquee>
 
-<img src="https://thisbucketisforterraform.s3.ap-south-1.amazonaws.com/ricknmorty.jpg" alt="Girl in a jacket" width="600" height="600">
+<img src="https://this-bucket-is-for-terraform.s3.ap-south-1.amazonaws.com/ricknmorty.jpg" alt="Girl in a jacket" width="600" height="600">
 
 </body>
 </html>
